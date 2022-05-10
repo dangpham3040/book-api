b3d0d3dd24abea7069d64b6536af49fcc393cb86
dangpham3040 <dangphamk18@gmail.com>
2022-05-10 19:20:17 +0700

    test fix

--- server.js
+++ server.js
@@ -7,4 +7,7 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+// server.listen(port);
+server.listen(port, function(){
+    console.log(`Application is listening on port ${port}!`)
+  });
